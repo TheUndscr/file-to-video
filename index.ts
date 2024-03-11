69e9faab4b4bbf8c4d65719f2ec27fdc1fc392a6
undscr <jgttpmail@gmail.com>
2024-03-11 12:51:39 -0500

    Updated visuals

--- index.ts
+++ index.ts
@@ -11,28 +11,26 @@ async function main() {
     const outputFilePath = outputPath + '/' + outputFileName + '-filetovid.mp4';
     
     console.clear();
-    console.log(`Preparing to output video to ${chalk.yellow(outputPath + '/' + outputFileName + '-filetovid.mp4')}...`);
+    console.log(`Outputting video to ${chalk.yellow(outputPath + '/' + outputFileName + '-filetovid.mp4')}...`);
 
     let totalTime = 0;
     ffmpeg()
         .input(inputPath)
         .inputOptions([
-            '-f rawvideo',
-            '-pixel_format rgba',
-            '-video_size 128x128',
-            '-framerate 30',
-        ])
-        .input(inputPath)
-        .inputOptions([
             '-f u8',
             '-ar 44100',
-            '-ac 1'
+            '-ac 2',
+        ])
+        .filterGraph([
+            'avectorscope=s=640x640:mode=lissajous_xy:draw=dot:scale=lin:zoom=1,format=yuv420p',
         ])
         .outputOptions([
+            '-c:v libx264',
             '-sws_flags fast_bilinear'
         ])
         .saveToFile(outputFilePath)
         .on('end', () => {
+            console.clear()
             console.log(chalk.green('File converted successfully!'));
         })
         .on('codecData', (data) => {
@@ -40,13 +38,13 @@ async function main() {
         })
         .on('error', (err) => { 
             console.error('Error converting file: ', err);
-        }).on('progress', (progress) => {
+        })
+        .on('progress', (progress) => {
             const time = parseInt(progress.timemark.replace(/:/g, ''));
+            const percent = (time / totalTime) * 100;
 
-            const percent = (time / totalTime);
-            process.stdout.write(`Processing: ${chalk.blue(percent.toFixed(2))}%\r`)
+            process.stdout.write(`Processing: ${chalk.blue(percent.toFixed(2))}%\r`);
         });
 }
-
 
 main();
